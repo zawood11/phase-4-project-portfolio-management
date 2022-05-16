0a8db848b2cc52e30668d02915bbcca33fed50e0
Zach Underwood <zawood11@gmail.com>
2022-05-16 17:49:23 -0400

    add routes for resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :recipes, only: [:index, :create]
+    resources :portfolios
+    resources :positions
+    resources :prices, only: [:index, :show, :create]
+    resources :stocks, except: [:update]
+    resources :users
+
     post "/signup", to: "users#create"
     get "/me", to: "users#show"
     post "/login", to: "sessions#create"
