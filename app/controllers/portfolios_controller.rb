4232aa773a9222099d1f039a968285387fc79856
Zach Underwood <zawood11@gmail.com>
2022-05-18 11:55:44 -0400

    comment out authorization in application controller temporarily, start routes in resource controllers

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,7 @@
 class PortfoliosController < ApplicationController
+    #GET /portfolios
+    def index
+        portfolios = Portfolio.all
+        render json: portfolios
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  namespace :api do
-    resources :portfolios
-    resources :positions
-    resources :prices, only: [:index, :show, :create]
-    resources :stocks, except: [:update]
-    resources :users
+  # namespace :api do
+  #   post "/signup", to: "users#create"
+  #   get "/me", to: "users#show"
+  #   post "/login", to: "sessions#create"
+  #   delete "/logout", to: "sessions#destroy"
+  # end
 
-    post "/signup", to: "users#create"
-    get "/me", to: "users#show"
-    post "/login", to: "sessions#create"
-    delete "/logout", to: "sessions#destroy"
-  end
+  resources :portfolios
+  resources :positions
+  resources :prices, only: [:index, :show, :create]
+  resources :stocks, except: [:update]
+  resources :users
+
   # all other routes will be load our React application
   # this route definition matches:
   # - *path: all paths not matched by one of the routes defined above
