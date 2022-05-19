00cb734e4be991d6d9909c31c75cd818e5e42d3f
Zach Underwood <zawood11@gmail.com>
2022-05-19 12:01:06 -0400

    add position controller methods, tested via postman

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -3,7 +3,8 @@ class PositionsController < ApplicationController
 
     #POST "/positions"
     def create
-
+        @position = Position.create!(position_params)
+        render json: @position, status: :created
     end
 
     #GET "/positions/:id"
@@ -13,17 +14,23 @@ class PositionsController < ApplicationController
 
     #PATCH "/positions/:id"
     def update
-
+        @position&.update!(position_params)
+        render json: @position, status: :accepted
     end
 
     #DELETE "/positions/:id"
     def destroy
-
+        @position&.destroy
+        render json: { message: "Position id: #{@position.id} has been deleted successfully." }
     end
 
     private
 
     def find_position
         @position = Position.find(params[:id])
+    end
+
+    def position_params
+        params.permit(:portfolio_id, :stock_id, :quantity)
     end
 end
