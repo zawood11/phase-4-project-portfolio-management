e322bd371ca6d08ea7cc23d44c92fe637c286473
Zach Underwood <zawood11@gmail.com>
2022-05-23 16:34:04 -0400

    add dependent destroy on stock controller, has many positions association

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,6 @@
 class Stock < ApplicationRecord
     has_many :prices, dependent: :destroy
-    has_many :positions
+    has_many :positions, dependent: :destroy
     has_many :portfolios, through: :positions
 
     validates :symbol, uniqueness: true
