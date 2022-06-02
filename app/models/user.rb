d2f503213d5056881a5ca2703e4b992760ad8681
Zach Underwood <zawood11@gmail.com>
2022-06-02 18:30:48 -0400

    add username length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   
   has_secure_password
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: {minimum: 4}
   validates :password, length: {minimum: 5}
 end
